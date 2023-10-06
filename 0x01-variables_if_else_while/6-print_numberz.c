d1db42cbb7107908182a047cb5454955d2ac8bd6
Ezz Morgan <morganezz226@gmail.com>
2023-10-07 02:13:39 +0300

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+  * main - prints numbers from 0 till 9
+  *
+  * Return: 0 Alwyas
+  */
+int main(void)
+{
+	int n = 48;
+
+	while (n < 58)
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+
+	return (0);
+}
