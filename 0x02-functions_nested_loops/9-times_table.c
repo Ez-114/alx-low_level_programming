a7dd673395d024a8b8d3aaa90af4ee9f9d868bdc
Ezz Morgan <morganezz226@gmail.com>
2023-10-14 14:08:35 +0300

    9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+void times_table(void)
+{
+	int i = 0, j = 0, result;
+
+	while (i <= 9)
+	{
+		j = 0;
+		while (j <= 9)
+		{
+			result = i * j;
+
+			if (result > 9)
+			{
+				_putchar('0' + result / 10);
+				_putchar('0' + result % 10);
+			}
+			else
+			{
+				_putchar('0' + result);
+			}
+
+			if (j < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+
+				if (((j + 1) * i) <= 9)
+				{
+					_putchar(' ');
+				}	
+			}
+
+			j++;
+		}
+		i++;
+		_putchar('\n');
+	}
+}
+
