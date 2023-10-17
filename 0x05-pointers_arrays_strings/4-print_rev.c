55f1447094cdadfdc2fcbf956fb1fcf059d8a07d
Ezz Morgan <morganezz226@gmail.com>
2023-10-17 23:47:04 +0300

    reverser

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_rev - prints string in reverse order
+ * @s: passed string as a pointer
+*/
+
+void print_rev(char *s)
+{
+	int i = 0;
+
+	/* to get the size of the array of characters */
+	while (*(s + i) != '\0')
+	{
+		i++;
+	}
+
+	/* now go from i to 0 to print in reverse where i is the number of chars */
+	while (i >= 0)
+	{
+		_putchar(*(s + i));
+		i--;
+	}
+	_putchar('\n');
+}
