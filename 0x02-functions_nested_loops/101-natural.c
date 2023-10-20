fe89d97a15eb733f8f5b56e3eda1bf4519bd7b18
Ezz Morgan <morganezz226@gmail.com>
2023-10-20 21:13:13 +0300

    multies

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+void multiples(int n);
+
+/**
+ * main - test a function
+ *
+ * Return: 0 (Success)
+*/
+
+int main(void)
+{
+	multiples(5);
+
+	return (0);
+}
+
+/**
+ * multiples - prints sum of multiples of 5 and 3
+ * @n: integer
+*/
+
+void multiples(int n)
+{
+	int i, sum = 0;
+
+	if (n == 1024)
+	{
+		return;
+	}
+
+	for (i = 0; i <= n; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+		{
+			sum = sum + i;
+		}
+	}
+
+	printf("%d\n", sum);
+}
