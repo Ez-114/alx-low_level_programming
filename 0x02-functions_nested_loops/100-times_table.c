4b8c45d0453d678378211a20c238419f07469d87
Ezz Morgan <morganezz226@gmail.com>
2023-10-20 20:38:30 +0300

    first advanced

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,69 +8,71 @@ void print_times_table(int n)
 {
 	int rows, cols, result;
 
-	/* check constrains for n */
-	if (n > 15 || n < 0)
+	rows = result = 0;
+
+	if (n >= 0 && n <= 15)
+	{
+		while (rows <= n)
+		{
+			cols = 1;
+
+			/**
+			 * print initial result
+			 * and some punctuation
+			*/
+			_putchar('0');
+			_putchar(',');
+
+			while (cols <= n)
+			{
+				/* calculates the times table */
+				result = rows * cols;
+
+				/* print result */
+				if (result <= 9)
+				{
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar('0' + result);
+
+					/* check if we are at last col */
+					if (cols != n)
+						_putchar(',');
+				}
+				else if (result > 9 && result <= 99)
+				{
+					_putchar(' ');
+					_putchar(' ');
+					_putchar('0' + (result / 10));
+					_putchar('0' + (result % 10));
+
+					/* check if we are at last col */
+					if (cols != n)
+						_putchar(',');
+				}
+				else if (result > 99)
+				{
+					_putchar(' ');
+					_putchar('0' + (result / 100));
+					_putchar('0' + ((result % 100) / 10));
+					_putchar('0' + (result % 10));
+
+					/* check if we are at last col */
+					if (cols != n)
+						_putchar(',');
+				}
+
+				cols++;
+			}
+			_putchar('\n');
+			rows++;
+		}
+	}
+	else
 	{
 		return;
 	}
-
-	rows = result = 0;
-	while (rows <= n)
-	{
-		cols = 1;
-
-		/**
-		 * print initial result
-		 * and some punctuation
-		*/
-		_putchar('0');
-		_putchar(',');
-
-		while (cols <= n)
-		{
-			/* calculates the times table */
-			result = rows * cols;
-
-			/* print result */
-			if (result <= 9)
-			{
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar('0' + result);
-
-				/* check if we are at last col */
-				if (cols != n)
-					_putchar(',');
-			}
-			else if (result > 9 && result <= 99)
-			{
-				_putchar(' ');
-				_putchar(' ');
-				_putchar('0' + (result / 10));
-				_putchar('0' + (result % 10));
-
-				/* check if we are at last col */
-				if (cols != n)
-					_putchar(',');
-			}
-			else if (result > 99)
-			{
-				_putchar(' ');
-				_putchar('0' + (result / 100));
-				_putchar('0' + ((result % 100) / 10));
-				_putchar('0' + (result % 10));
-
-				/* check if we are at last col */
-				if (cols != n)
-					_putchar(',');
-			}
-
-			cols++;
-		}
-		_putchar('\n');
-		rows++;
-	}
-
+	
 	_putchar('\n');
 }
