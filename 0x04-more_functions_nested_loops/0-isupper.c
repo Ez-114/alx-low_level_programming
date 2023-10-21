5eebb90d063581b3e351f2c3cc93dad5db481c1a
Ezz Morgan <morganezz226@gmail.com>
2023-10-21 14:04:18 +0300

    more functions and nested loops

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+/**
+ * isupper - check if the given char is an upper alpha
+ * @c: input char
+ *
+ * Return: 1 (Success) 0 (Fail)
+*/
+int isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+
+	return (0);
+}
