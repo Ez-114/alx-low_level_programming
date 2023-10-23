94e21a6537664d9746d8b8a741b9f496e6a1165d
Ezz Morgan <morganezz226@gmail.com>
2023-10-23 22:55:40 +0300

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,19 +1,20 @@
-#include "main.h"
+#include <string.h>
 
 /**
  * rev_string - reverse string in its value
  * @s: passed string
 */
-
 void rev_string(char *s)
 {
-	int i = 0;
-	char *rev = s;
+	/* get the size of the string */
+	int size = strlen(s), i = 0;
+	char temp;
 
-	while (s[i])
+	while (i < (size / 2))
 	{
+		temp = s[i];
+		s[i] = s[size - 1 - i];
+		s[size - 1 - i] = temp;
 		i++;
 	}
-
-	_putchar(rev);
 }
