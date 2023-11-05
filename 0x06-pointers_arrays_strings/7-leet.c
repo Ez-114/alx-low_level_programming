4ac19b89549e4db62e11410e5de58ca5b6068151
Ezz Morgan <morganezz226@gmail.com>
2023-11-05 16:07:07 +0200

    7-leet.c

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,28 @@
+/**
+  * leet - encodes a string into 1337
+  * @str: pointer to the passed string
+  *
+  * Example leet("Hello") --> H3110
+  * Return: encoded string
+  */
+char *leet(char *str)
+{
+	char *ptr = str;
+	char key[] = {'A', 'E', 'O', 'T', 'L'};
+	int val[] = {4, 3, 0, 7, 1};
+	int i;
+
+	while (*str != '0')
+	{
+		for (i = 0; i < sizeof(key) / sizeof(char); i++)
+		{
+			if (*str == key[i] || *str == key[i] + 32)
+			{
+				*str = 48 + val[i];
+			}
+		}
+		str++;
+	}
+	return (ptr);
+}
+
