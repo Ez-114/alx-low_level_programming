4e5db00ae2e17eb7af8323f9afbdacd98f40af13
Ezz Morgan <morganezz226@gmail.com>
2023-11-05 16:48:08 +0200

    100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -7,43 +7,22 @@
 char *rot13(char *str)
 {
 	char *ptr = str;
-	char letter[] =
-	{
-		'a', 'b', 'c', 'd',
-		'e', 'f', 'g', 'h',
-		'i', 'j', 'k', 'l',
-		'm', 'n', 'o', 'p',
-		'q', 'r', 's', 't',
-		'u', 'v', 'w', 'x',
-		'y', 'z'
-	};
-	int replace[] =
-	{
-		'n', 'o', 'p', 'q',
-		'r', 's', 't', 'u',
-		'v', 'w', 'x', 'y',
-		'z', 'a', 'b', 'c',
-		'd', 'e', 'f', 'g',
-		'h', 'i', 'j', 'k',
-		'l', 'm'
-	};
-	int i;
+	char base;
+	int offset = 13;
 
 	while (*str != '\0')
 	{
-		for (i = 0; i < 26; i++)
+		if (*str >= 'a' && *str <= 'z')
 		{
-			if (*str == letter[i])
-			{
-				*str = replace[i];
-				break;
-			}
-			else if (*str == (letter[i] - 32))
-			{
-				*str = replace[i] - 32;
-				break;
-			}
+			base = 'a';
 		}
+		else if (*str >= 'A' && *str <= 'Z')
+		{
+			base = 'A';
+		}
+
+		*str = ((*str - base + offset) % 26) + base;
+
 		str++;
 	}
 	return (ptr);
