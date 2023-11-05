d188e135339484abb120b984accab0001b4b295d
Ezz Morgan <morganezz226@gmail.com>
2023-11-05 16:55:40 +0200

    100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -8,16 +8,16 @@ char *rot13(char *str)
 {
 	char *ptr = str;
 	char in13[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-	char out13[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm"
+	char out13[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 	int i;
 
 	while (*str != '\0')
 	{
 		for (i = 0; i < 52; i++)
 		{
-			if (*s == in13[i])
+			if (*str == in13[i])
 			{
-				*s = out13[i];
+				*str = out13[i];
 				break;
 			}
 		}
