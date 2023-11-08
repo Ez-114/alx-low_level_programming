60219046dd0a66e27da7cf3c9e991b381c7f1963
Ezz Morgan <morganezz226@gmail.com>
2023-11-08 12:07:53 +0200

    7-print_chessboard.c

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,17 @@
+/**
+ * print_chessboard - a function that prints the chessboard.
+ * @a: pointer to a 2D array
+*/
+void print_chessboard(char (*a)[8])
+{
+	int row, col;
+
+	for (row = 0; row < 8; row++)
+	{
+		for (col = 0; col < 8; col++)
+		{
+			printf("%s", a[row][col]);
+		}
+		printf("\n");
+	}
+}
