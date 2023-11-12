44db4525529b6d25ca1dfe8fa8e5312ba0c63e42
Ezz Morgan <morganezz226@gmail.com>
2023-11-12 23:29:23 +0200

    mul

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - print result of multiplication of 2 ints
+ * @argc: arg count
+ * @argv: arg value
+ *
+ * Return: 0
+*/
+int main(int argc, char *argv[])
+{
+	int num1, num2;
+
+	if (argc == 1)
+	{
+		printf("ERROR\n");
+		return (0);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+
+	printf("%d\n", num1 * num2);
+
+	return (0);
+}
