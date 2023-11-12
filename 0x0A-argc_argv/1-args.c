23a22fa8d192173fe20c89652823fec1e0dfe45d
Ezz Morgan <morganezz226@gmail.com>
2023-11-12 23:18:15 +0200

    argc

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - print the number of args passed
+ * @argc: arg count
+ * @argv: arg value
+ *
+ * Return: 0
+*/
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("%d\n", argc);
+	return (0);
+}
