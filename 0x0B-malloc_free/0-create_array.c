5851401c709606d6cfaf2aaec8c89c22e27dcb7f
Ezz Morgan <morganezz226@gmail.com>
2023-11-14 15:32:28 +0200

    fix

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,7 +15,7 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 
 	str = malloc(sizeof(*str) * size);
-	if (str == NULL)
+	if (str == NULL ||  size == 0)
 		return (NULL);
 
 	for (i = 0; i < size; i++)
