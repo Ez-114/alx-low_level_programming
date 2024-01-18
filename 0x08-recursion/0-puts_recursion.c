5ec1d2ce2a1f0e292e04b32dca171b2c0ab78bed
Ezz Morgan <morganezz226@gmail.com>
2024-01-18 18:38:39 +0200

    Handeled the empty string bug

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,19 +1,20 @@
 #include "main.h"
+#include <string.h>
 
 /**
   * _puts_recursion - A function that prints a string recursively.
-  * s: A character pointer to the passed stirng.
+  * @s: A character pointer to the passed stirng.
   */
 void _puts_recursion(char *s)
 {
-	if (*s == '\0')
+	if (*s == '\0' || strlen(s) == 0)
 		return;
-	else
-	{
-		_putchar(*s);
-		s++;
-		if (*s == '\0')
-			_putchar('\n');
-		_puts_recursion(s);
-	}
+
+	_putchar(*s);
+	s++;
+
+	if (*s == '\0')
+		_putchar('\n');
+
+	_puts_recursion(s);
 }
