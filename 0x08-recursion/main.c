a3feafa40105551a27e2e80bd27b70c5f116c0f1
Ezz Morgan <morganezz226@gmail.com>
2024-01-18 20:35:38 +0200

    task 1

--- 0x08-recursion/main.c
+++ 0x08-recursion/main.c
@@ -7,6 +7,6 @@
 */
 int main(void)
 {
-    _puts_recursion("");
+    _print_rev_recursion("\nHello");
     return (0);
 }
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -3,5 +3,6 @@
 
 int _putchar(int);
 void _puts_recursion(char *);
+void _print_rev_recursion(char *);
 
 #endif
