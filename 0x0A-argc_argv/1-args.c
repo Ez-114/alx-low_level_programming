5dcfb72e7d0a438eead018513961ec2a9f914b81
ez-114 <morganezz226@gmail.com>
2024-01-25 17:10:42 +0200

    task 2

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+* main - driver code, prints the number of args passed
+* @argc: number of arguments passed
+* @argv: vector of arguments passed
+*
+* Return: (0) success
+*/
+int main(int argc, char __attribute__((unused)) *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
