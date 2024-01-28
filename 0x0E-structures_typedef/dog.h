67406cb66cf2c4b8e3dfe60b1cd63c7c2011b71a
ez-114 <morganezz226@gmail.com>
2024-01-28 20:19:37 +0200

    task 1

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+* struct dog - a dog struct data type
+* @name: name of the dog
+* @age: dog age
+* @owner: owner of the dog
+*
+* Description: A  struct containing some info about dog names, age and owner.
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif
