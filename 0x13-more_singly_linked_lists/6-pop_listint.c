a8d6c8826dadf1f60ec5ea33296115c252028806
ez-114 <morganezz226@gmail.com>
2024-02-06 18:51:08 +0200

    0x13-linked_list: task (6) popping the head node from a list.

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+* pop_listint - deletes the head node of a list and return its data
+* @head: head of the list
+*
+* Return: the data of the popped node, (0) if list is empty
+*/
+int pop_listint(listint_t **head)
+{
+	int n = 0;
+	listint_t *current_node = NULL;
+
+	if (*head == NULL)
+		return (0);
+	
+	current_node = *head;
+	n = current_node->n;
+	*head = current_node->next;
+	free(current_node);
+
+	return (n);
+}
