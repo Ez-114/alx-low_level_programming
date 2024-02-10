0182714ec65b33ae07cc645290ec4256786e1c31
ez-114 <morganezz226@gmail.com>
2024-02-10 19:18:03 +0200

    0x14-bit_manipulation: task (4) set bit.

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+
+/**
+* set_bit - sets the value of a bit to 1 at a given index
+* @n: passed number by reference
+* @index: given index of the bit
+*
+* Return: (1) Success, (-1) Error
+*/
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index >= sizeof(n) * 8 || n == NULL)
+		return (-1);
+
+	*n = *n | (1 << index);
+	return (1);
+}
