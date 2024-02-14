06bffe8d4a99257cd7c438fd43b1e9f5bdc41a46
ez-114 <morganezz226@gmail.com>
2024-02-14 16:55:27 +0200

    0x15-file_io: task (4) copy content

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,114 @@
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * copy_contents - copy the content from one file to another
+ * @fd_from: file descriptor of source file
+ * @fd_to: file descriptor of destination file
+ *
+ * Return: 1 on success
+*/
+int copy_contents(int fd_from, int fd_to)
+{
+	ssize_t bytes_read = 0, bytes_write = 0;
+	char *buffer = NULL;
+
+	/* allocate space to the buffer */
+	buffer = malloc(1024 * sizeof(char));
+	if (buffer == NULL)
+	{
+		dprintf(STDERR_FILENO, "Error: Buffer allocation failure.\n");
+		close(fd_from);
+		close(fd_to);
+		exit(101);
+	}
+
+	/* read from file_from 1024 byte and write them to file_to */
+	do {
+		bytes_read = read(fd_from, buffer, sizeof(buffer));
+		bytes_write = write(fd_to, buffer, bytes_read);
+
+		/* error handling */
+		if (bytes_write != bytes_read || bytes_read == -1 || bytes_write == -1)
+		{
+			free(buffer);
+			return (100);
+		}
+
+	} while (bytes_read > 0);
+
+	free(buffer);
+	return (1);
+}
+
+/**
+ * cp_file - copies content from one file to another
+ * @file_from: file to take text content from
+ * @file_to: file to put text content in
+ *
+ * Return: 1 on Success, -1 on failure
+*/
+int cp_file(char *file_from, char *file_to)
+{
+	/* init variables */
+	int fd_from, fd_to, r_val;
+
+	/* check for files */
+	if (file_from == NULL || file_to == NULL)
+		return (-1);
+
+	/* try open file_from */
+	fd_from = open(file_from, O_RDONLY);
+	if (fd_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+		exit(98);
+	}
+
+	/* try open file_to */
+	fd_to = open(file_to, O_WRONLY | O_TRUNC | O_CREAT, 0664);
+	if (fd_to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
+		close(fd_from);
+		exit(99);
+	}
+
+	r_val = copy_contents(fd_from, fd_to);
+	if (r_val == 100)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
+		close(fd_from);
+		close(fd_to);
+		exit(100);
+	}
+
+	/* On success close files and free buffer */
+	close(fd_from);
+	close(fd_to);
+	return (1);
+}
+
+/**
+ * main - driver code
+ * @ac: argument count
+ * @av: argument vector
+ *
+ * Return: 0 on success
+*/
+int main(int ac, char **av)
+{
+	int res;
+
+	if (ac != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	res = cp_file(av[1], av[2]);
+	printf("-> %i)\n", res);
+	return (0);
+}
