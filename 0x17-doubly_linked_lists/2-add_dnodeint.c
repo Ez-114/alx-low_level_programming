569d9049800dc0fa383b5955efabc6aa515fcaa9
ez-114 <morganezz226@gmail.com>
2024-04-11 23:47:13 +0200

    MODIFYfix memmory allocation size in add_dnodeint function

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -12,7 +12,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	dlistint_t *new_node = NULL;
 
 	/* Allocate & confirm allocation is success */
-	new_node = malloc(sizeof(dlistint_t *));
+	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
 		return (NULL);
 
