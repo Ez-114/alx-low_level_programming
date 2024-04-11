89b0ffad85ad129b80c704437f2053ec067b5b14
ez-114 <morganezz226@gmail.com>
2024-04-11 23:23:11 +0200

    ADD: task 01 completed

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - calculates the length of a ll
+ * @h: head of a list
+ * Return: Length of the ll
+*/
+size_t dlistint_len(const dlistint_t *h)
+{
+    int size = 0;
+
+	if (h == NULL)
+		return (0);
+
+	while (h != NULL)
+	{
+		h = h->next;
+		size++;
+	}
+	return (size);
+}
