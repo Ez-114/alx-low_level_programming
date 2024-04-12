8aa09e540d701f976c714ef99e52bbd7cfb738a3
ez-114 <morganezz226@gmail.com>
2024-04-12 16:22:14 +0200

    MODIFY: changed Kernel style code in 6-sum_dlistint.c file

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * sum_dlistint - Calculates the sum of all the data (n) of 
+ * sum_dlistint - Calculates the sum of all the data (n) of
  * a dlistint_t linked list.
  * @head: head of passed dll
  * Return: the sum of all the data (n), 0 if the list is empty
